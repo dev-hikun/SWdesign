476770ed2e8c89445866ad7e01fa40335253bb26
Choiseonghwan <jiryu147@naver.com>
2017-09-23 02:42:44 +0900

    [SportsClub - App] TabView 일부 기능 구현 #4

--- SWdesign/app/SportsClub/app/src/main/java/com/club/sports/sportsclub/home/SportsClubHomeActivity.java
+++ SWdesign/app/SportsClub/app/src/main/java/com/club/sports/sportsclub/home/SportsClubHomeActivity.java
@@ -1,18 +1,33 @@
 package com.club.sports.sportsclub.home;
 
+import android.app.TabActivity;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TabHost;
+import android.widget.TextView;
 
 import com.club.sports.sportsclub.R;
 import com.club.sports.sportsclub.back.BackPressCloseHandler;
+import com.club.sports.sportsclub.tab.TabContestInfoActivity;
+import com.club.sports.sportsclub.tab.TabGroupActivity;
+import com.club.sports.sportsclub.tab.TabMyInfoActivity;
+import com.club.sports.sportsclub.tab.TabRankActivity;
 
 /**
  * Created by again on 2017-09-12.
  */
 
-public class SportsClubHomeActivity extends AppCompatActivity{
+public class SportsClubHomeActivity extends TabActivity{
 
     private BackPressCloseHandler mBackPressCloseHandler;
+    private TabHost mTabHost;
+    private TabHost.TabSpec mSpec;
+    private Intent mIntent;
+    private ImageView mTopLogoImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +39,65 @@ public class SportsClubHomeActivity extends AppCompatActivity{
 
     private void initialize(){
         mBackPressCloseHandler = new BackPressCloseHandler(this);
+        findView();
+        initializeTab();
+        onClickTopLogoImageView();
+    }
+
+    private void findView(){
+        mTopLogoImageView = (ImageView) findViewById(R.id.top_logo);
+    }
+
+    private void initializeTab(){
+        mTabHost = getTabHost();
+
+        applyIntentContest();
+        applyIntentGroup();
+        applyIntentRank();
+        applyIntentMyInfo();
+        applyIntentTextColor();
+    }
+
+    private void onClickTopLogoImageView(){
+        mTopLogoImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mIntent = new Intent().setClass(SportsClubHomeActivity.this, SportsClubHomeActivity.class);
+                startActivity(mIntent);
+                finish();
+            }
+        });
+    }
+
+    private void applyIntentContest(){
+        mIntent = new Intent().setClass(this, TabContestInfoActivity.class);
+        mSpec = mTabHost.newTabSpec("Contest").setIndicator("대회 정보", getResources().getDrawable(R.drawable.contest_info)).setContent(mIntent);
+        mTabHost.addTab(mSpec);
+    }
+
+    private void applyIntentGroup(){
+        mIntent = new Intent().setClass(this, TabGroupActivity.class);
+        mSpec = mTabHost.newTabSpec("Group").setIndicator("클럽", getResources().getDrawable(R.drawable.group)).setContent(mIntent);
+        mTabHost.addTab(mSpec);
+    }
+
+    private void applyIntentRank(){
+        mIntent = new Intent().setClass(this, TabRankActivity.class);
+        mSpec = mTabHost.newTabSpec("Rank").setIndicator("랭킹", getResources().getDrawable(R.drawable.rank)).setContent(mIntent);
+        mTabHost.addTab(mSpec);
+    }
+
+    private void applyIntentMyInfo(){
+        mIntent = new Intent().setClass(this, TabMyInfoActivity.class);
+        mSpec = mTabHost.newTabSpec("MyInfo").setIndicator("내 정보", getResources().getDrawable(R.drawable.my)).setContent(mIntent);
+        mTabHost.addTab(mSpec);
+    }
+
+    private void applyIntentTextColor(){
+        for(int i = 0; i<getTabWidget().getChildCount(); i++) {
+            TextView textView = (TextView) mTabHost.getTabWidget().getChildAt(i).findViewById(android.R.id.title);
+            textView.setTextColor(Color.BLACK);
+        }
     }
 
     @Override
---
+++ SWdesign/app/SportsClub/app/src/main/java/com/club/sports/sportsclub/tab/TabContestInfoActivity.java
@@ -0,0 +1,23 @@
+package com.club.sports.sportsclub.tab;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+/**
+ * Created by again on 2017-09-23.
+ */
+
+public class TabContestInfoActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TextView tv = new TextView(this);
+        tv.setText("대회 정보 내용");
+        tv.setTextSize(22);
+        tv.setTextColor(Color.BLUE);
+        setContentView(tv);
+    }
+}
