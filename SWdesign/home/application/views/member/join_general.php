7906577540c615e91e4c8588fa437c9525371996
heehyeon <hihyeoni@naver.com>
2017-10-10 01:13:35 +0900

    [Web] 일반회원가입 퍼블리싱 및 개발 초기작업 #1

--- SWdesign/home/application/views/member/join_general.php
+++ SWdesign/home/application/views/member/join_general.php
@@ -2,6 +2,50 @@
 <article class="title">
     <h2 class="join">일반회원 가입</h2>
     <div>
-        <span>위런에 오신 것을 환영합니다. 회원 가입 후 위런만을 위한 다양한 혜택을 누리세요.</span>
+        <span>&nbsp;</span>
     </div>
 </article>
+
+<nav class="lnb join">
+    <ul class="three">
+        <li class="on">01 회원약관동의</li>
+        <li>02 회원정보 입력</li>
+        <li>03 가입완료</li>
+    </ul>
+</nav>
+
+<div class="termsForm">
+    <div class="agreeHeader">
+        <div class="agreeText">
+            <p>위런에 오신 것을 환영합니다. 회원 가입 후 위런 회원만을 위한 다양한 혜택을 누리세요.</p>
+            <p>위런 회원 가입을 위하여 아래 회원약관 및 개인정보처리방침 등을 확인하시고 동의하여 주시기 바립니다.</p>
+        </div>
+        <div class="agreeCheck">
+            <label for="is_agree">
+                <input type="checkbox" id="is_agree" name="is_agree" />전체 약관에 동의합니다
+            </label>
+        </div>
+    </div>
+    <div class="termsHeader">
+        <p class="tit">위런 회원 이용약관(필수)</p>
+    </div>
+    <div class="termsBox">
+        이용약관 내용이 들어가는 부분
+    </div>
+    <div class="termsCheck">
+        <label for="is_agree1">
+            <input type="checkbox" id="is_agree1" name="is_agree1" />위런 회원약관에 동의합니다.
+        </label>
+    </div>
+    <div class="termsHeader">
+        <p class="tit">개인정보 수집, 제공 및 활용 동의</p>
+    </div>
+    <div class="termsBox">
+        개인정보 수집, 제공 및 활용 동의 내용이 들어가는 부분
+    </div>
+    <div class="termsCheck">
+        <label for="is_agree2">
+            <input type="checkbox" id="is_agree2" name="is_agree2" />위런의 개인정보 수집, 제공 및 활용에 동의합니다.
+        </label>
+    </div>
+</div>
