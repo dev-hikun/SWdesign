60fb2606fb65e03f65d6a24ea9c3fbb133242ec4
Hee-hyeon Lee <hihyeoni@naver.com>
2017-11-10 19:30:08 +0900

    aws release test2

--- SWdesign/home/application/config/routes.php
+++ SWdesign/home/application/config/routes.php
@@ -52,4 +52,4 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'welcome';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
-$route['(:num)'] = "sport/$1";
+$route['(.+)'] = "$1";
