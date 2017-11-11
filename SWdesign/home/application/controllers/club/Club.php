1c699c6b0c41b75f999a5cc46a02d2cc7459c208
Hee-hyeon Lee <hihyeoni@naver.com>
2017-11-11 15:20:37 +0900

    클럽 등록 백단 작업 임시저장

--- SWdesign/home/application/controllers/club/Club.php
+++ SWdesign/home/application/controllers/club/Club.php
@@ -1,35 +1,19 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
 class Club extends CI_Controller {
-        public function __construct() {
-            parent::__construct();
-            $this->load->helper(array('form', 'url'));
-        }
+  public function __construct() {
+      parent::__construct();
+      $this->load->helper(array('form', 'url'));
+  }
 
-        public function _remap($mode, $arg="")
-        {
-          //변수 설정
-          $data['page_title'] = '클럽';
-          $data['css_link'] = '<link href="/libraries/css/club.css" rel="stylesheet" type="text/css" />';
-          $data['mode'] = $mode;
-          $this->load->view('templates/header', $data); //헤더 인클루드
-          switch($mode){
-            case 'regist': $this->_regist($mode); break;
-            default:  $this->load->view('club/list', $data); break;
-          }
-          $this->load->view('templates/footer'); //푸터 인클루드
-        }
-
-        /* 클럽 등록 */
-        public function _regist($mode){
-          if(!isset($_SESSION['logged_in'])){
-            echo "<script type='text/javascript'>
-              alert('해당 서비스를 이용하기 위해서는 로그인이 필요합니다.');
-              document.location.href='/member/login?ref=club'
-            </script>";
-            exit;
-          }
-          $this->load->view('club/regist', $mode);
-        }
+  public function index()
+  {
+    //변수 설정
+    $data['page_title'] = '클럽리스트';
+    $data['css_link'] = '<link href="/libraries/css/club.css" rel="stylesheet" type="text/css" />';
+    $data['mode'] = 'list';
+    $this->load->view('templates/header', $data); //헤더 인클루드
+    $this->load->view('club/list', $data);
+    $this->load->view('templates/footer'); //푸터 인클루드
+  }
 }
+?>
