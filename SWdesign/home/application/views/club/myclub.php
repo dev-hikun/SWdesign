bcee2323d9adfb7eab637882a780d9ada07157c4
Hee-hyeon Lee <hihyeoni@naver.com>
2017-11-17 01:25:47 +0900

    [WEB] club 개발 #27 - 나의 클럽 최초 커밋.

--- SWdesign/home/application/views/club/myclub.php
+++ SWdesign/home/application/views/club/myclub.php
@@ -1,8 +1,12 @@
 <article class="title">
     <h2>나의 클럽</h2>
     <div>
-        <span>클럽을 개설하여 가까운 사람들과 함께하세요!</span>
+        <span>가입한 클럽들을 보고, 사람들과 소통을 나누세요!</span>
     </div>
 </article>
 
 <?php $this->load->view('club/clubNav'); ?>
+
+<article>
+준비중입니다.
+</article>
